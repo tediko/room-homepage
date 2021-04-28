ca9765cc4613414c72628077dc6b0bcff43d246a
tediko <tediko123@wp.pl>
2021-04-28 14:59:40 +0200

    Added changeInfo to slider to change kv__intro content

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -8,34 +8,35 @@ export default class Slider {
         this.selectors = {
             backgrounds: 'data-bg',
             buttons: 'data-btn',
+            intro: 'data-intro',
+            introHeading: 'data-intro-heading',
+            introText: 'data-intro-text',
             slideRightClass: 'slide-right',
-            slideLeftClass: 'slide-left'
+            slideLeftClass: 'slide-left',
+            slideClass: 'slide'
         }
 
         this.products = {
-            1: {
+            0: {
                 title: `Discover innovative ways to decorate`,
-                desc: `We provide unmatched quality, comfort, and style for property owners across the country. 
-                Our experts combine form and function in bringing your vision to life. 
-                Create a room in your own style with our collection and make your property a reflection of you and what you love.`
+                desc: `We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.`
+            },
+            1: {
+                title: `We are available all across the globe`,
+                desc: `With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.`
             },
             2: {
-                title: `We are available all across the globe`,
-                desc: `With stores all over the world, it's easy for you to find furniture for your home or place of business. 
-                Locally, we’re in most major cities throughout the country. Find the branch nearest you using our 
-                store locator. Any questions? Don't hesitate to contact us today.`
-            },
-            3: {
                 title: `Manufactured with the best materials`,
-                desc: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology 
-                to ensure that every product is made as perfect and as consistent as possible. With three decades of 
-                experience in this industry, we understand what customers want for their home and office.`
+                desc: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.`
             }
         }
 
         this.backgrounds = document.querySelectorAll(`[${this.selectors.backgrounds}]`);
         this.buttons = document.querySelectorAll(`[${this.selectors.buttons}]`);
-        if (!this.backgrounds || !this.buttons) return false;
+        this.intro = document.querySelector(`[${this.selectors.intro}]`);
+        this.introHeading = document.querySelector(`[${this.selectors.introHeading}]`);
+        this.introText = document.querySelector(`[${this.selectors.introText}]`);
+        if (!this.backgrounds || !this.buttons || !this.intro || !this.introHeading || !this.introText) return false;
 
         this.index = 0;
         this.previousIndex = 0;
@@ -63,7 +64,17 @@ export default class Slider {
         this.backgrounds[this.index].style.zIndex = '3';
         this.backgrounds[this.previousIndex].style.zIndex = '2';
 
+        this.changeInfo()
         this.watchForAnimationend();
+    }
+
+    changeInfo() {
+        this.intro.classList.add(`${this.selectors.slideClass}`);
+
+        setTimeout(() => {
+            this.introHeading.innerText = `${this.products[this.index].title}`;
+            this.introText.innerText = `${this.products[this.index].desc}`;
+        }, 500)
     }
 
     watchForAnimationend() {
@@ -71,6 +82,9 @@ export default class Slider {
             bg.addEventListener('animationend', () => {
                 this.backgrounds[this.previousIndex].classList.remove(`${this.selectors.slideRightClass}`, `${this.selectors.slideLeftClass}`);
                 this.backgrounds[this.previousIndex].style.zIndex = '';
+
+                this.intro.classList.remove(`${this.selectors.slideClass}`);
+
                 this.enabled = true;
             })
         })
